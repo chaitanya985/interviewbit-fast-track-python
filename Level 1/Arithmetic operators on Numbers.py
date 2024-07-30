1a6c0afdd58244fe4302a0678ffdb6c21ecab3b6
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-07-30 19:47:50 +0530

    Arithmetic operators on Numbers

---
+++ Level 1/Arithmetic operators on Numbers.py
@@ -0,0 +1,17 @@
+https://www.interviewbit.com/problems/arithmetic-operators-on-numbers/
+
+def main():
+    A = int(input())
+    B = int(input())
+    # Print seven lines as described above
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(A/B)
+    print(A%B)
+    print(A**B)
+    return 0
+
+if __name__ == '__main__':
+    main()
